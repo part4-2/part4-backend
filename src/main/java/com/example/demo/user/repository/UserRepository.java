939397337f638c4c0817ff90d847dd6fc30c8b8b
cmong0516 <cmong0516@gmail.com>
2024-02-02 09:46:37 +0900

    refactor[UserRepository]: findByEmail -> findByOauthId

--- src/main/java/com/example/demo/user/repository/UserRepository.java
+++ src/main/java/com/example/demo/user/repository/UserRepository.java
@@ -2,11 +2,10 @@ package com.example.demo.user.repository;
 
 import com.example.demo.user.domain.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
+
 public interface UserRepository extends JpaRepository<Users,Long> {
-    Optional<Users> findByEmail(String email);
+    Optional<Users> findByOauthId(String id);
 }
