251159ff6ef13bcfb4e5d8063cf4f5b4babc7097
cmong0516 <cmong0516@gmail.com>
2024-02-05 17:11:33 +0900

    fix[OAuth2Controller]: add URL Decode

--- src/main/java/com/example/demo/oauth/controller/OAuth2Controller.java
+++ src/main/java/com/example/demo/oauth/controller/OAuth2Controller.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "OAuth2Controller" , description = "소셜 로그인 컨트롤러")
@@ -21,6 +24,10 @@ public class OAuth2Controller {
     @Operation(summary = "소셜로그인",description = "소셜 로그인을 통한 회원가입")
     @GetMapping("/login/oauth/{provider}")
     public ResponseEntity<LoginResponse> login(@PathVariable String provider, @RequestParam String code) {
+
+        if (provider.equals("google")) {
+            code = URLDecoder.decode(code, StandardCharsets.UTF_8);
+        }
         LoginResponse loginResponse = oAuth2Service.login(provider, code);
         return ResponseEntity.ok().body(loginResponse);
     }
