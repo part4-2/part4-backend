c24b505540aef340f5531f1f5e7d717741240fe4
cmong0516 <cmong0516@gmail.com>
2024-02-06 13:35:10 +0900

    refactor[UserService]: add 닉네임 중복 체크, 회원 정보 수정

--- src/main/java/com/example/demo/user/service/UserService.java
+++ src/main/java/com/example/demo/user/service/UserService.java
@@ -2,7 +2,9 @@ package com.example.demo.user.service;
 
 import com.example.demo.jwt.CustomUserDetails;
 import com.example.demo.user.domain.entity.Users;
-import com.example.demo.user.domain.request.UserInfoUpdateRequest;
+import com.example.demo.user.domain.request.RequiredUserInfoRequest;
+import com.example.demo.user.domain.request.UpdateUserRequest;
+import com.example.demo.user.domain.response.UpdateUserResponse;
 import com.example.demo.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -14,11 +16,25 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional
-    public Users updateUser(CustomUserDetails customUserDetails, UserInfoUpdateRequest userInfoUpdateRequest) {
+    public UpdateUserResponse requiredUserInfo(CustomUserDetails customUserDetails, RequiredUserInfoRequest requiredUserInfoRequest) {
         Users users = customUserDetails.getUsers();
-        users.updateEssentials(userInfoUpdateRequest);
+        users.updateEssentials(requiredUserInfoRequest);
+
         userRepository.save(users);
 
-        return users;
+        return new UpdateUserResponse(users);
+    }
+
+    public UpdateUserResponse updateUser(CustomUserDetails customUserDetails, UpdateUserRequest updateUserRequest) {
+        Users users = customUserDetails.getUsers();
+        users.updateUserInfo(updateUserRequest);
+
+        userRepository.save(users);
+
+        return new UpdateUserResponse(users);
+    }
+
+    public boolean checkNickName(String nickName) {
+        return userRepository.findByNickName(nickName).isEmpty();
     }
 }
