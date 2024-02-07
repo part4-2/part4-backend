6d78a1e2f07a1fcc41a74cee1b280a318a5f44be
DK <laancer4@gmail.com>
2024-02-07 21:21:44 +0900

    feat : ExceptionHandler

---
+++ src/main/java/com/example/demo/global/presentation/ErrorResponse.java
@@ -0,0 +1,4 @@
+package com.example.demo.global.presentation;
+
+public record ErrorResponse(String message) {
+}
--- src/main/java/com/example/demo/user/exception/UserException.java
+++ src/main/java/com/example/demo/user/exception/UserException.java
@@ -14,5 +14,9 @@ public class UserException extends RuntimeException{
         public UserNotFoundException(String email) {
             super(String.format("조회한 멤버가 존재하지 않습니다. - request info { email : %s }", email));
         }
+
+        public UserNotFoundException(){
+            super("사용자 인증에 실패했습니다. 다시 로그인을 시도 해 주세요.");
+        }
     }
 }
