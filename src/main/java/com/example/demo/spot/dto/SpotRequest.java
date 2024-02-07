fa141f517adbe2256a352586504aab338e675fdb
DK <laancer4@gmail.com>
2024-02-07 21:22:28 +0900

    fix : id는 DB측에서 생성하도록 변경

--- src/main/java/com/example/demo/spot/dto/SpotRequest.java
+++ src/main/java/com/example/demo/spot/dto/SpotRequest.java
@@ -3,7 +3,6 @@ package com.example.demo.spot.dto;
 import com.example.demo.spot.domain.vo.Location;
 
 public record SpotRequest(
-        Long id,
         String displayName, // "소플러스 제주점~"
         String formattedAddress,// "대한민국 ~도 ~시 ~로 111"
         Location location
