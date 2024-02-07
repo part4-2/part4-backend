5032e2db8dec8f75255ca6a28ec63a2ad5306091
DK <laancer4@gmail.com>
2024-02-07 21:48:48 +0900

    refactor[Review] : 스웨거 설명 추가 및 @Valid 어노테이션에 대응하는 @NotBlank 어노테이션 추가

--- src/main/java/com/example/demo/review/application/dto/ReviewWriteRequest.java
+++ src/main/java/com/example/demo/review/application/dto/ReviewWriteRequest.java
@@ -1,8 +1,13 @@
 package com.example.demo.review.application.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record ReviewWriteRequest(
+        @NotBlank(message = "제목은 빈 값일 수 없습니다.")
         String title,
+        @NotBlank(message = "내용은 빈 값일 수 없습니다.")
         String content,
+        @NotBlank(message = "날씨는 빈 값일 수 없습니다.")
         String weatherDescription
 ) {
 
--- src/main/java/com/example/demo/review/presentation/ReviewController.java
+++ src/main/java/com/example/demo/review/presentation/ReviewController.java
@@ -5,6 +5,7 @@ import com.example.demo.review.application.ReviewService;
 import com.example.demo.review.application.dto.ReviewRequest;
 import com.example.demo.review.application.dto.ReviewResponseDTO;
 import com.example.demo.review.application.dto.ReviewWriteRequest;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -19,7 +20,9 @@ import java.net.URI;
 @Log4j2
 public class ReviewController {
     private final ReviewService reviewService;
+
     @PostMapping("/api/spots/{spotId}/reviews")
+    @Operation(summary = "리뷰 쓰기" , description = "리뷰를 작성합니다.")
     public ResponseEntity<Void> writeReview(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
             @RequestBody @Valid ReviewWriteRequest reviewWriteRequest,
@@ -40,6 +43,7 @@ public class ReviewController {
 
     // TODO: 2/7/24 spotId 빼도 될수도?
     @GetMapping("/api/spots/{spot-id}/reviews")
+    @Operation(summary = "리뷰 조회" , description = "리뷰 id에 해당하는 리뷰 정보를 불러옵니다.")
     public ResponseEntity<ReviewResponseDTO> getReview(@PathVariable("spot-id") Long spotId,
                                                        @RequestParam("review-id") Long reviewId){
         final ReviewResponseDTO oneById = reviewService.getOneById(reviewId);
@@ -47,6 +51,7 @@ public class ReviewController {
     }
 
     @PutMapping("/api/spots/{spot-id}/reviews/{review-id}")
+    @Operation(summary = "리뷰 수정" , description = "리뷰를 수정합니다. 수정 시 모든 컬럼의 정보가 필요합니다.")
     public ResponseEntity<Void> updateReview(@PathVariable("spot-id") Long spotId,
                                                        @RequestBody @Valid ReviewWriteRequest reviewRequest,
                                                        @PathVariable("review-id") Long reviewId){
