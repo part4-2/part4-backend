b2424e05f092261a5546585fb90d277f7a8ce79f
DK <laancer4@gmail.com>
2024-02-07 22:11:13 +0900

    feat[Review] : 리스트 조회 추가

--- src/main/java/com/example/demo/review/presentation/ReviewController.java
+++ src/main/java/com/example/demo/review/presentation/ReviewController.java
@@ -5,6 +5,8 @@ import com.example.demo.review.application.ReviewService;
 import com.example.demo.review.application.dto.ReviewRequest;
 import com.example.demo.review.application.dto.ReviewResponseDTO;
 import com.example.demo.review.application.dto.ReviewWriteRequest;
+import com.example.demo.review.domain.vo.SearchCondition;
+import com.example.demo.review_tag.domain.vo.TagName;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +16,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -42,12 +46,12 @@ public class ReviewController {
     }
 
     // TODO: 2/7/24 spotId 빼도 될수도?
-    @GetMapping("/api/spots/{spot-id}/reviews")
+    @GetMapping("/api/spots/{spot-id}/reviews/{review-id}")
     @Operation(summary = "리뷰 조회" , description = "리뷰 id에 해당하는 리뷰 정보를 불러옵니다.")
     public ResponseEntity<ReviewResponseDTO> getReview(@PathVariable("spot-id") Long spotId,
-                                                       @RequestParam("review-id") Long reviewId){
-        final ReviewResponseDTO oneById = reviewService.getOneById(reviewId);
-        return ResponseEntity.ok(oneById);
+                                                       @PathVariable("review-id") Long reviewId){
+        final ReviewResponseDTO result = reviewService.getOneById(reviewId);
+        return ResponseEntity.ok(result);
     }
 
     @PutMapping("/api/spots/{spot-id}/reviews/{review-id}")
@@ -57,5 +61,21 @@ public class ReviewController {
                                                        @PathVariable("review-id") Long reviewId){
         reviewService.updateReview(reviewId,reviewRequest);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/api/spots/{spot-id}/reviews")
+    @Operation(summary = "리뷰 조회(리스트)" , description = "태그, 조회 조건(좋아요, 최신순)에 따라 리뷰들을 조회합니다.")
+    public ResponseEntity<List<ReviewResponseDTO>> getReviews(
+            @PathVariable("spot-id") Long spotId,
+            @RequestParam List<String> tagNames,
+            @RequestParam SearchCondition searchCondition
+                                                              ){
+        List<TagName> tags = new ArrayList<>();
+        if (!tagNames.isEmpty()){
+            tags = tagNames.stream().map(TagName::new).toList();
+        }
+
+        List<ReviewResponseDTO> result = reviewService.findByTagNames(tags, searchCondition);
+        return ResponseEntity.ok(result);
     }
 }
