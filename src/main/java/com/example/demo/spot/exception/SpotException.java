27690b9af4bb74e7ef7290cf7a256e0cc26626f8
DK <laancer4@gmail.com>
2024-02-09 20:08:41 +0900

    test : Location

--- src/main/java/com/example/demo/spot/exception/SpotException.java
+++ src/main/java/com/example/demo/spot/exception/SpotException.java
@@ -1,6 +1,6 @@
 package com.example.demo.spot.exception;
 
-public class SpotException extends RuntimeException{
+public class SpotException extends RuntimeException {
     public SpotException(String message) {
         super(message);
     }
@@ -19,6 +19,24 @@ public class SpotException extends RuntimeException{
 
         public DuplicatedSpotExistsException(String formattedAddress) {
             super(String.format("중복된 주소입니다. - request info { formattedAddress : %s }", formattedAddress));
+        }
+    }
+
+    public static class LocationLengthException extends SpotException {
+        public LocationLengthException(final int allowedLength, final String latitude, final String longitude) {
+            super(String.format(
+                            "좌표 입력값이 너무 깁니다. - {allowedLength : %d, latitude_length : %d, longitude_length : %d }",
+                            allowedLength,
+                            latitude.length(),
+                            longitude.length()
+                    )
+            );
+        }
+    }
+
+    public static class LocationBlankException extends SpotException {
+        public LocationBlankException() {
+            super("좌표 중 빈 값이 있습니다.");
         }
     }
 }
