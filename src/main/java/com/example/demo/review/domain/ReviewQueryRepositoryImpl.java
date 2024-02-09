3c59a02f9bbc71da7d74f953dcd763fe842444dd
DK <laancer4@gmail.com>
2024-02-09 13:59:27 +0900

    feat[Review] :좋아요 순으로 정렬하는 쿼리 생성

--- src/main/java/com/example/demo/review/domain/ReviewQueryRepositoryImpl.java
+++ src/main/java/com/example/demo/review/domain/ReviewQueryRepositoryImpl.java
@@ -7,7 +7,6 @@ import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -21,24 +20,45 @@ import static com.example.demo.review_tag.domain.QTag.tag;
 @RequiredArgsConstructor
 public class ReviewQueryRepositoryImpl implements ReviewQueryRepository {
     private final JPAQueryFactory queryFactory;
-    // 한방쿼리
 
+    // TODO: 2/9/24 이 쿼리 leftJoin 처리하면 결과가 나올 것임. 그러나 태그 엔티티 작업 후 테스트하기로...
     @Override
-    @Transactional(readOnly = true)
-    public List<Review> findByTagNames(List<TagName> tagNames, SearchCondition searchCondition) {
-        return queryFactory
-                .select(review)
-                .distinct()
-                .from(review)
-                .innerJoin(review, reviewTag.review)
-                .innerJoin(reviewTag.tag, tag)
-                .where(
-                        this.searchByTagNames(tagNames)
-                        )
+    public List<Review> findByTagNames(List<TagName> tags, SearchCondition searchCondition){
+        List<Long> taggedReviews = this.findTaggedReviews(tags);
+        return queryFactory.selectFrom(review)
+                .innerJoin(reviewTag)
+                .on(review.id.eq(reviewTag.review.id))
+                .where(searchByTagNamesIfPresent(taggedReviews))
+                .innerJoin(reviewLike)
+                .on(reviewLike.review.id.eq(review.id))
                 .groupBy(review)
                 .orderBy(this.getOrder(searchCondition))
                 .fetch();
     }
+
+    @Override
+    public List<Review> findByLikes() {
+        return queryFactory.selectFrom(review)
+                .leftJoin(review.reviewLikes, reviewLike)
+                .groupBy(review.id) // 리뷰의 id로 그룹화하여
+                .orderBy(reviewLike.count().desc(), review.createdDate.desc()) // reviewLike의 개수로 내림차순 정렬
+                .limit(20) // 상위 20개 리뷰만 선택
+                .fetch();
+    }
+
+    private List<Long> findTaggedReviews(List<TagName> tags){
+        return queryFactory.select(reviewTag.id)
+                .from(reviewTag)
+                .innerJoin(tag)
+                .on(tag.id.eq(reviewTag.tag.id))
+                .where(searchByTagNames(tags))
+                .fetch();
+    }
+
+    private BooleanExpression searchByTagNamesIfPresent(List<Long> taggedReviews){
+        return taggedReviews == null || taggedReviews.isEmpty() ? null : reviewTag.id.in(taggedReviews);
+    }
+
     private BooleanExpression searchByTagNames(List<TagName> tagNames){
         return tagNames == null || tagNames.isEmpty() ? null : tag.name.in(tagNames);
     }
