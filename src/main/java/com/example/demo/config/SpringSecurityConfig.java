ffee11247b79ce5d19f8c9e2f6bfd30e6c9dee93
DK <laancer4@gmail.com>
2024-02-13 22:42:25 +0900

    feat : 시큐리티 퍼밋올

--- src/main/java/com/example/demo/config/SpringSecurityConfig.java
+++ src/main/java/com/example/demo/config/SpringSecurityConfig.java
@@ -34,8 +34,9 @@ public class SpringSecurityConfig {
                         .requestMatchers(antMatcher("/api/user/**")).hasRole("USER")
                         .requestMatchers(antMatcher("/api/admin/**")).hasRole("ADMIN")
                         .requestMatchers("/","/index.html","/error","/login/oauth/**","/swagger-ui/**","/v3/api-docs/**","/swagger-resources/**","/api/nickname/**").permitAll()
-                        .anyRequest().authenticated()))
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider) , UsernamePasswordAuthenticationFilter.class);
+                        .anyRequest().permitAll()));
+//                        .anyRequest().authenticated()));
+//                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider) , UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
 
