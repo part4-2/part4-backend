5605d62106bfe0977add2618ae44f93e058e6030
DK <laancer4@gmail.com>
2024-02-15 16:23:01 +0900

    fix : @ModelAttribute 활용해서 받도록 변경

--- src/main/java/com/example/demo/review/application/dto/ReviewWriteRequest.java
+++ src/main/java/com/example/demo/review/application/dto/ReviewWriteRequest.java
@@ -1,8 +1,12 @@
 package com.example.demo.review.application.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
 
 public record ReviewWriteRequest(
         @NotBlank(message = "제목은 빈 값일 수 없습니다.")
@@ -11,8 +15,15 @@ public record ReviewWriteRequest(
         String content,
         TagValues tagValues,
         String visitingTime,
-        Double starRank
+        Double starRank,
+        List<MultipartFile> images
 ) {
+        @Override
+        public List<MultipartFile> images() {
+                if (Objects.isNull(images)) {
+                        return Collections.emptyList();
+                }
 
-
+                return images;
+        }
 }
