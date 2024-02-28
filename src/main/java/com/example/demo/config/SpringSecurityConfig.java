3391d05d4b6443e9dfc3c311f32594e2fa6e67fa
cmong0516 <cmong0516@gmail.com>
2024-02-28 17:31:37 +0900

    refactor[Cors]: fidCors

--- src/main/java/com/example/demo/config/SpringSecurityConfig.java
+++ src/main/java/com/example/demo/config/SpringSecurityConfig.java
@@ -34,9 +34,7 @@ public class SpringSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(corsCustomizer -> corsCustomizer.configurationSource(request -> {
                     CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowedOrigins(Collections.singletonList("/https://www.trimo.kr"));
-                    config.setAllowedOrigins(Collections.singletonList("*"));
-                    config.setAllowedOrigins(Collections.singletonList("/https://www.trimo.kr/"));
+                    config.setAllowedOrigins(Collections.singletonList("https://www.trimo.kr"));
                     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
                     config.setAllowedHeaders(Collections.singletonList("*"));
                     config.setExposedHeaders(List.of("*"));
