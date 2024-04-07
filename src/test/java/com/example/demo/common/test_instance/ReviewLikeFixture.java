64ed5691e6099c1cbb4a3a13e8757eb0fe4b16bc
seinHan <sein151@naver.com>
2024-04-07 17:09:43 +0900

    test : 세인 ReviewLike test code 작성

--- src/test/java/com/example/demo/common/test_instance/ReviewLikeFixture.java
+++ src/test/java/com/example/demo/common/test_instance/ReviewLikeFixture.java
@@ -5,12 +5,9 @@ import com.example.demo.review_like.domain.vo.ReviewLikeId;
 import com.example.demo.user.domain.entity.vo.UserId;
 
 public class ReviewLikeFixture {
-    private final static Long TEST_USERID = 1L;
-    private final static Long TEST_REVIEW_ID = 1L;
-
     public static final ReviewLikeId REVIEW_LIKE_OF_ALL_FIELD_VALUE_IS_FOO = new ReviewLikeId(
-            new UserId(TEST_USERID),
-            TEST_REVIEW_ID
+            new UserId(1L),
+            1L
     );
 
     public static final ReviewLikeId REVIEW_LIKE_ID_BY_DK = new ReviewLikeId(new UserId(1L), 1L);
@@ -23,6 +20,11 @@ public class ReviewLikeFixture {
     public static final ReviewLike REVIEW_LIKE_BY_IH = new ReviewLike(
             new UserId(2L),
             2L
+    );
+
+    public static final ReviewLike REVIEW_LIKE_BY_SI = new ReviewLike(
+            new UserId(3L),
+            1L
     );
 
 }
